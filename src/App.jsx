e9092d7c0b2ceb5122b51184834706fa9efc1de5
Carl Joseph <carljoseph156@gmail.com>
2025-01-19 19:31:09 -0500

    Updated Delete Book and added route to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
 import "./App.css";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateBook />} />
+        <Route path="/delete/:id" element={<DeleteBook />} />
       </Routes>
     </>
   );
