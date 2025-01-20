9a0ebe6f29d10207599e6a24cb4f47768c45d3f6
Carl Joseph <carljoseph156@gmail.com>
2025-01-20 10:51:35 -0500

    Added/Updated App with imports and routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,24 @@ import CreateBook from "./pages/CreateBook";
 import DeleteBook from "./pages/DeleteBook";
 import ShowBook from "./pages/ShowBook";
 import UpdateBook from "./pages/UpdateBook";
+import BookList from "./pages/BookList";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
   return (
     <>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/booklist" element={<BookList />} />
         <Route path="/create" element={<CreateBook />} />
         <Route path="/book/:id" element={<ShowBook />} />
         <Route path="/update/:id" element={<UpdateBook />} />
         <Route path="/delete/:id" element={<DeleteBook />} />
       </Routes>
+      <Footer />
     </>
   );
 };
