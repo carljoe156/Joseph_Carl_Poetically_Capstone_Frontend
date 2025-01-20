edd660d33f511b9f55e9161ca4e17a6aade86313
Carl Joseph <carljoseph156@gmail.com>
2025-01-19 19:43:07 -0500

    Updated Update Book and added routing to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import CreateBook from "./pages/CreateBook";
 import DeleteBook from "./pages/DeleteBook";
+import ShowBook from "./pages/ShowBook";
+import UpdateBook from "./pages/UpdateBook";
 import "./App.css";
 
 const App = () => {
@@ -12,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreateBook />} />
         <Route path="/book/:id" element={<ShowBook />} />
+        <Route path="/update/:id" element={<UpdateBook />} />
         <Route path="/delete/:id" element={<DeleteBook />} />
       </Routes>
     </>
