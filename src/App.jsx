c902cf51d3dd68295be42d6ece272dad2aaa4af8
Carl Joseph <carljoseph156@gmail.com>
2025-01-19 19:25:48 -0500

    Updated CreateBook and adding route to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
 import "./App.css";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateBook />} />
       </Routes>
     </>
   );
