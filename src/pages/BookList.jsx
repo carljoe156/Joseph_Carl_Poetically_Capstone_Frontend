de9930c9eaf3baab9eef5b24fba343b293fb3c45
Carl Joseph <carljoseph156@gmail.com>
2025-01-20 10:49:52 -0500

    Added Book List and rerouted lists from Home page

---
+++ src/pages/BookList.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import Spinner from "../components/Spinner";
+import BooksCard from "../components/home/BooksCard";
+import BooksTable from "../components/home/BooksTable";
+
+const BookList = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("card");
+  const LOCAL_URL = "http://localhost:5000";
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(`${LOCAL_URL}/api/v1/allbooks`, {
+          withCredentials: true,
+        }); // supposed work but idk
+        // const res = await axios.get("http://localhost:5000");
+        //console.log('API Response:', res.data);
+        console.log(res.data);
+        setBooks(res.data.data); // Update books with the data property from the response
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        setBooks([]); // Set books to an empty array on error
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+  console.log("books object:", books);
+
+  return (
+    <div className="p-4">
+      <header className="flex justify-between items-center">
+        <h1 className="text-3xl font-semibold text-gray-800">Books List</h1>
+        <Link to="/create">
+          <button className="flex items-center bg-green-500 hover:bg-green-600 text-white font-semibold px-3 py-1 rounded-lg">
+            <MdOutlineAddBox className="text-xl mr-1" />
+            Add Book
+          </button>
+        </Link>
+      </header>
+
+      <div className="mt-4 flex justify-center items-center gap-x-4">
+        <button
+          className={`${
+            showType === "table" ? "bg-sky-600" : "bg-sky-300 hover:bg-sky-600"
+          } px-4 py-1 rounded-lg text-white font-semibold`}
+          onClick={() => setShowType("table")}
+        >
+          Table View
+        </button>
+        <button
+          className={`${
+            showType === "card" ? "bg-sky-600" : "bg-sky-300 hover:bg-sky-600"
+          } px-4 py-1 rounded-lg text-white font-semibold`}
+          onClick={() => setShowType("card")}
+        >
+          Card View
+        </button>
+      </div>
+
+      {loading ? (
+        <Spinner />
+      ) : showType === "card" ? (
+        <BooksCard books={books} />
+      ) : (
+        <BooksTable books={books} />
+      )}
+    </div>
+  );
+};
+
+export default BookList;
